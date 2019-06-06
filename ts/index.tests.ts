ace9b5961ab1702bfe4da9afb3d8868001060c50
Vincent den Boer <vincent@shishkabab.net>
2019-06-06 10:32:59 +0200

    Updated test

--- ts/index.tests.ts
+++ ts/index.tests.ts
@@ -856,7 +856,7 @@ export function testStorageBackendOperations(backendCreator : StorexBackendTestB
             expect(foundObjectAfterUpdate).toEqual([{
                 id: newObject.id,
                 fieldString: 'new test',
-                fieldRandomKey: 'Found: Stored: Found: Stored: undefined',
+                fieldRandomKey: 'Found: Stored: undefined',
             }])
         })
     })
