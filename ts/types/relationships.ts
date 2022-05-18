2cf6340b6ea009c1f65b371d2c12a47adafe63ba
Vincent den Boer <vincent@youapt.eu>
2022-05-18 11:30:13 +0200

    Add `optional` field to (single)ChildOf relationships

--- ts/types/relationships.ts
+++ ts/types/relationships.ts
@@ -8,6 +8,7 @@ export interface ChildOfRelationship extends RelationshipType {
     fieldName?: string
     reverseAlias?: string
     single?: boolean
+    optional?: boolean
 }
 
 export interface MultipleChildOfRelationship extends ChildOfRelationship {
@@ -17,10 +18,10 @@ export interface SingleChildOfRelationship extends ChildOfRelationship {
     singleChildOf: string
 }
 export const isChildOfRelationship =
-    (relationship) : relationship is ChildOfRelationship =>
+    (relationship): relationship is ChildOfRelationship =>
         !!(<MultipleChildOfRelationship>relationship).childOf ||
         !!(<SingleChildOfRelationship>relationship).singleChildOf
-export const getChildOfRelationshipTarget = (relationship : ChildOfRelationship) =>
+export const getChildOfRelationshipTarget = (relationship: ChildOfRelationship) =>
     (<SingleChildOfRelationship>relationship).singleChildOf ||
     (<MultipleChildOfRelationship>relationship).childOf
 
@@ -31,9 +32,9 @@ export interface ConnectsRelationship extends RelationshipType {
     reverseAliases?: [string, string]
 }
 export const isConnectsRelationship =
-    (relationship : Relationship) : relationship is ConnectsRelationship =>
+    (relationship: Relationship): relationship is ConnectsRelationship =>
         !!(<ConnectsRelationship>relationship).connects
-export const isConnectsCollection = (relationships : Relationships) => {
+export const isConnectsCollection = (relationships: Relationships) => {
     for (const relationship of relationships) {
         if (isConnectsRelationship(relationship)) {
             return true
@@ -43,11 +44,11 @@ export const isConnectsCollection = (relationships : Relationships) => {
     return false
 }
 export const getOtherCollectionOfConnectsRelationship =
-    (relationship : ConnectsRelationship, thisCollection) =>
+    (relationship: ConnectsRelationship, thisCollection) =>
         relationship.connects[relationship.connects[0] == thisCollection ? 1 : 0]
 
 export type Relationship = SingleChildOfRelationship | MultipleChildOfRelationship | ConnectsRelationship
 export type Relationships = Relationship[]
-export type RelationshipsByAlias = {[alias : string] : Relationship}
-export type RelationshipReference = {relationship : string}
-export const isRelationshipReference = (reference) : reference is RelationshipReference => !!(<RelationshipReference>reference).relationship
+export type RelationshipsByAlias = { [alias: string]: Relationship }
+export type RelationshipReference = { relationship: string }
+export const isRelationshipReference = (reference): reference is RelationshipReference => !!(<RelationshipReference>reference).relationship
